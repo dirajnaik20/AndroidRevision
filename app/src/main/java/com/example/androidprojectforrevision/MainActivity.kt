8afc098a44b683c069554d7f517b75ac35c3c543
Diraj Naik <dirajnaik2020@gmail.com>
2023-07-21 19:30:20 +0530

    done2

--- app/src/main/java/com/example/androidprojectforrevision/MainActivity.kt
+++ app/src/main/java/com/example/androidprojectforrevision/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.androidprojectforrevision
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnCustomToast).setOnClickListener {
+            Toast(this).also {
+                Toast.LENGTH_LONG
+         it.view = layoutInflater.inflate(R.layout.custom_toast_layout,findViewById<ConstraintLayout>(R.id.clCustomToast),false)
+                it.show()
+            }
+        }
     }
 }
