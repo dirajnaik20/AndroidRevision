a640a15a27d141c5373b6fd6c7cee13970f48ba9
Diraj Naik <dirajnaik2020@gmail.com>
2023-07-23 19:41:36 +0530

    Done

--- app/src/main/java/com/example/androidprojectforrevision/MainActivity.kt
+++ app/src/main/java/com/example/androidprojectforrevision/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.androidprojectforrevision
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnGetImage).setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type="image/*"
+                startActivityForResult(it,0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==Activity.RESULT_OK && requestCode==0){
+            val uri=data?.data
+            findViewById<ImageView>(R.id.ivImage).setImageURI(uri)
+        }
     }
 }
